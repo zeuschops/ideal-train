9b3f383848fe76b4bcbbfdbd9e7e0c34f64d2a29
Austin Bennett <56428356+zeuschops@users.noreply.github.com>
2021-01-25 14:42:42 -0800

    Added Music funtionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import discord
 from discord.ext import commands
 from commands.VersionRequests import VersionRequests
+from commands.Music import Music
 
 bot = commands.Bot(command_prefix="??")
 
@@ -11,6 +12,7 @@ f.close()
 
 @bot.event
 async def on_ready():
+    bot.add_cog(Music(bot))
     bot.add_cog(VersionRequests(bot))
     print("Logged in as {0.user}".format(bot))
     print("\twith client id {0.user.id}".format(bot))
@@ -22,5 +24,9 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.channel.send(str(int(round(bot.latency * 1000,0))) + " ms")
+
+@bot.command()
+async def github(ctx):
+    await ctx.channel.send("See me on GitHub!\nhttps://github.com/zeuschops/ideal-train")
 
 bot.run(config['token'], bot=True, reconnect=True)
